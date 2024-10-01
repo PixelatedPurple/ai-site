a74f69c2f6e1c744444c7db62e7ade99b9b11e85
Dep1xel <85250250+PixelatedPurple@users.noreply.github.com>
2024-10-01 07:42:17 +0000

    Update promote_user.php

--- forum/api/promote_user.php
+++ forum/api/promote_user.php
@@ -2,7 +2,7 @@
 session_start();
 require 'config.php';
 
-if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] != 1) {
+if (!isset($_SESSION['is_owner']) || $_SESSION['is_owner'] != 1) {
     header('Location: login.php');
     exit();
 }
