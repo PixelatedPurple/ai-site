7d1ad1c552d3ccc71408563726858e6c4b05e03f
Dep1xel <85250250+PixelatedPurple@users.noreply.github.com>
2024-10-01 07:48:53 +0000

    Update config.php

--- forum/config.php
+++ forum/config.php
@@ -1,8 +1,13 @@
 <?php
-$host = 'localhost';
-$db = 'forum_db';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+$host = 'localhost'; // or your database host
+$db = 'forum'; // your database name
+$user = 'root'; // your database username
+$pass = ''; // your database password
 
-$dsn = "mysql:host=$host;
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database $db :" . $e->getMessage());
+}
+?>
