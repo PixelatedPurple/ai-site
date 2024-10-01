683bc45ce8eecab33b797fef58d8321f1366ea9f
Dep1xel <85250250+PixelatedPurple@users.noreply.github.com>
2024-10-01 08:06:55 +0000

    Update config.php

--- forum/config.php
+++ forum/config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost'; // or your database host
-$db = 'forum'; // your database name
-$user = 'root'; // your database username
-$pass = ''; // your database password
+$host = 'sql.freedb.tech'; // or your database host
+$db = 'freedb_forumdata'; // your database name
+$user = 'freedb_Database_Worker'; // your database username
+$pass = 'c!PB@5QJvhtu%E5'; // your database password
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
